a066c386d524de595db843efb0cc30fdc51c1da0
DevRodi <rodi.control@gmail.com>
2025-04-22 21:40:07 -0600

    ajustando para  registro de  aspirantes  desde link x2

--- app/Http/Middleware/SetDatabaseByOrigin.php
+++ app/Http/Middleware/SetDatabaseByOrigin.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
 
@@ -10,27 +10,33 @@ class SetDatabaseByOrigin
 {
     public function handle($request, Closure $next)
     {
-        $origin = $request->getHost(); 
+        $origin = $request->getHost();
 
-        if (str_contains($origin, 'localhost') || str_contains($origin, '127.0.0.1')) {
-            // 🟢 Desarrollo local
-            Log::info('🌱 Usando configuración local');
+        // Nos aseguramos de que la conexión por defecto sea “mysql”
+        Config::set('database.default', 'mysql');
 
+        if (Str::contains($origin, ['127.0.0.1', 'localhost'])) {
+            Log::info('🌱 DB Local');
             Config::set('database.connections.mysql.database', env('DB_DATABASE_TEST'));
             Config::set('database.connections.mysql.username', env('DB_USERNAME_TEST'));
             Config::set('database.connections.mysql.password', env('DB_PASSWORD_TEST'));
 
-        } elseif (str_contains($origin, 'sandbox.talentsafecontrol.com') || str_contains($origin, 'pruebas.')) {
-            // 🔸 Entorno de pruebas
+        } elseif (Str::contains($origin, 'sandboxcandidatos.talentsafecontrol.com')) {
+            Log::info('🔸 DB Sandbox');
+            // No cambias host, asume localhost
             Config::set('database.connections.mysql.database', env('DB_DATABASE_SANDBOX'));
             Config::set('database.connections.mysql.username', env('DB_USERNAME_SANDBOX'));
             Config::set('database.connections.mysql.password', env('DB_PASSWORD_SANDBOX'));
 
-        } elseif (str_contains($origin, 'portal.talentsafecontrol.com')){ 
-            // 🔹 Producción
+        } elseif (Str::contains($origin, 'bolsatrabajo.talentsafecontrol.com')) {
+            Log::info('🔹 DB Producción');
+            // Estos valores ahora salen de DB_DATABASE*, etc.
             Config::set('database.connections.mysql.database', env('DB_DATABASE_PROD'));
             Config::set('database.connections.mysql.username', env('DB_USERNAME_PROD'));
             Config::set('database.connections.mysql.password', env('DB_PASSWORD_PROD'));
+        } else {
+            Log::warning("Host desconocido: {$origin}, usando DB por defecto.");
+            // deja las vars definidas en .env DB_DATABASE / DB_USERNAME / DB_PASSWORD
         }
 
         return $next($request);
